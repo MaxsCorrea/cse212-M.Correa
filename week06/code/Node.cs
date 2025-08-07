ec658f6b2632101d1d1f60b93d508b1e792665e2
Maximiliano Correa <correamaximiliano10@gmail.com>
2025-08-07 10:26:34 -0600

    adding code activity

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -11,19 +11,19 @@ public class Node
 
     public void Insert(int value)
     {
-        // TODO Start Problem 1
+        // Do not insert duplicates
+        if (value == Data)
+            return;
 
         if (value < Data)
         {
-            // Insert to the left
             if (Left is null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
-        else
+        else // value > Data
         {
-            // Insert to the right
             if (Right is null)
                 Right = new Node(value);
             else
@@ -33,13 +33,18 @@ public class Node
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+        else if (value < Data)
+            return Left != null && Left.Contains(value);
+        else // value > Data
+            return Right != null && Right.Contains(value);
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
